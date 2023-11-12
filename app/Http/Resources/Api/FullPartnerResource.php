19de764fde91b5e21672ad4fac452044e3b385ff
KarimEl-Kady <karim.elkady,kmk@gmail.com>
2023-11-12 15:19:47 +0200

    karim

--- app/Http/Resources/Api/FullPartnerResource.php
+++ app/Http/Resources/Api/FullPartnerResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\Api;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PartnerResource extends JsonResource
+class FullPartnerResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,14 +17,16 @@ class PartnerResource extends JsonResource
         return [
             "id"=>$this->id,
             "images" => ImageResource::collection($this->images) ?? "",
-            "name"=>$this->name ??"",
+            "name"=>$this->name??"",
             "age"=>$this->age??"",
             "trusted"=>$this->trusted??"",
-            "is_new" => intval($this->is_new),
+            "is_new"=>$this->is_new??"",
             "country"=>$this->country??"",
             "city"=>$this->city??"",
             "active"=>$this->active??"",
+            "partner_requirments"=>$this->partner_requirments??"",
             "partner_more_info"=>PartnerMoreInfo::collection($this->partner_more_info),
-        ];
+            "questions"=>PartnerMoreInfo::collection($this->questions),
+            ];
     }
 }
