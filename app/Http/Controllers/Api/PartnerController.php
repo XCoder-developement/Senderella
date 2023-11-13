d4827571cb8f782bbbebc5e6fe156ea6f68c62a5
KarimEl-Kady <karim.elkady,kmk@gmail.com>
2023-11-13 15:40:41 +0200

    karim

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -38,8 +38,6 @@ class PartnerController extends Controller
     {
         try {
 
-
-
             $partners =[
 
              "partner_id" => "required|exists:user,id",
@@ -48,7 +46,7 @@ class PartnerController extends Controller
             if ($validator->fails()) {
                 return $this->getvalidationErrors("validator");
             }
-            $partner= User::whereId($request->partner_id)->first();
+            $partner= User::whereId($request->user_id)->first();
 
             $msg="fetch_partner_details";
             return $this->dataResponse($msg, new PartnerResource($partner), 200);
@@ -57,4 +55,24 @@ class PartnerController extends Controller
         }
     }
 
+
+    public function fetch_new_partners()
+    {
+        try {
+
+            $user = auth()->user();
+
+            $partners = User::whereNot('id', auth()->id())->orderBy('id', 'desc')->paginate(10);
+            if (!$partners) {
+                $msg = "there is no partners";
+
+                return $this->errorResponse($msg, 401);
+            }
+            $msg = "fetch_all_users";
+
+            return $this->dataResponse($msg, PartnerResource::collection($partners)->response()->getData(true), 200);
+        } catch (\Exception $ex) {
+            return $this->returnException($ex->getMessage(), 500);
+        }
+    }
 }
