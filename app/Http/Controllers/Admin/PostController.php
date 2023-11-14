aab56e0f840ce38d8dd5976b6db1ffacaf229f79
unknown <ehabtalaat5552@gmail.com>
2023-11-14 11:05:40 +0200

    userAge

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Traits\ApiTrait;
 use App\Models\Post\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
+    use ApiTrait;
 
     public function index()
     {
-        //
+        try {
+            $posts = Post::orderBy("created_at", "desc")->paginate(10);
+            $msg = "fetch_posts_descending";
+            $data = PostResource::collection($posts);
+            return $this->dataResponse($msg, $data, 200);
+        } catch (\Exception $ex) {
+            return $this->returnException($ex->getMessage(), 500);
+        }
+
     }
 
 
@@ -49,5 +60,5 @@ class PostController extends Controller
     {
         //
     }
-    
+
 }
