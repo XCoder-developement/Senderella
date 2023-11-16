d8d9252efeeb234370e9d58b333d6d7ba630cc22
unknown <ehabtalaat5552@gmail.com>
2023-11-16 11:32:47 +0200

    filter

--- app/Http/Controllers/Api/SearchPartnerController.php
+++ app/Http/Controllers/Api/SearchPartnerController.php
@@ -15,65 +15,53 @@ class SearchPartnerController extends Controller
     public function search_partner(Request $request)
     {
         try {
-            $partner = User::where(function ($q) use ($request) {
-                $q->orwhere('state_id', $request->has('state_id'))
-                    ->orWhere('country_id', $request->country_id)
-                    ->orWhere('country_id', $request->nationality_id)
-                    ->orWhere('height', $request->height)
-                    ->orWhere('weight', $request->weight)
-                    ->orWhere('marital_status_id', $request->marital_status_id)
-                    ->orwhere('name', $request->word)
-                    ->ageRange($request->age_from, $request->age_to);
-            })->paginate(10);
-            $res = UserResource::collection($partner);
+            $partner = User::where('id', '!=', auth()->user()->id)
+                ->where(function ($q) use ($request) {
+                    $q->when($request->has('word'), function ($q) use ($request) {
+                        $q->orWhere('name', 'like', '%' . $request->word . '%');
+                    });
+                    $q->when($request->has('state_id') && $request->state_id, function ($q) use ($request) {
+                        $q->orwhere('state_id', $request->state_id);
+                    });
+                    $q->when($request->has('country_id'), function ($q) use ($request) {
+                        $q->orwhere('country_id', $request->country_id);
+                        $q->orwhere('country_id', $request->nationality_id);
+                    });
+                    $q->when($request->has('weight'), function ($q) use ($request) {
+                        $q->orwhere('weight', $request->weight);
+                    });
+                    $q->when($request->has('height'), function ($q) use ($request) {
+                        $q->orwhere('height', $request->height);
+                    });
+                    $q->when($request->has('marital_status_id'), function ($q) use ($request) {
+                        $q->orwhere('marital_status_id', $request->marital_status_id);
+                    });
+                    $q->when($request->has('age_from') && $request->has('age_to'), function ($q) use ($request) {
+                        $q->ageRange($request->age_from, $request->age_to);
+                    });
+                })->paginate(10);
+            $res = UserResource::collection($partner)->response()->getData(true);
             $msg = "search_partner";
             return $this->dataResponse($msg, $res, 200);
         } catch (\Exception $e) {
             return $this->returnException($e->getMessage(), 500);
         }
-        // try {
-        //     $query = User::query();
-
-        //     if ($request->has('state_id')) {
-        //         $query->orWhere('state_id', $request->state_id);
-        //     }
-
-        //     if ($request->has('country_id')) {
-        //         $query->orWhere('country_id', $request->country_id);
-        //     }
-
-        //     if ($request->has('nationality_id')) {
-        //         $query->orWhere('nationality_id', $request->nationality_id);
-        //     }
-
-        //     if ($request->has('height')) {
-        //         $query->orWhere('height', $request->height);
-        //     }
-
-        //     if ($request->has('weight')) {
-        //         $query->orWhere('weight', $request->weight);
-        //     }
-
-        //     if ($request->has('marital_status_id')) {
-        //         $query->orWhere('marital_status_id', $request->marital_status_id);
-        //     }
-
-        //     if ($request->has('word')) {
-        //         $query->orWhere('name', 'like', '%' . $request->word . '%');
-        //     }
-
-        //     if ($request->has('age_from') && $request->has('age_to')) {
-        //         $query->ageRange($request->age_from, $request->age_to);
-        //     }
-
-        //     $partner = $query->paginate(10);
-        //     $res = UserResource::collection($partner);
-        //     $msg = "filter_data";
-        //     return $this->dataResponse($msg, $res, 200);
-        // } catch (\Exception $e) {
-        //     return $this->returnException($e->getMessage(), 500);
-        // }
-
     }
-
+    // try {
+    //     $partner = User::where(function ($q) use ($request) {
+    //         $q->orwhere('state_id', $request->has('state_id'))
+    //             ->orWhere('country_id', $request->country_id)
+    //             ->orWhere('country_id', $request->nationality_id)
+    //             ->orWhere('height', $request->height)
+    //             ->orWhere('weight', $request->weight)
+    //             ->orWhere('marital_status_id', $request->marital_status_id)
+    //             ->orwhere('name', $request->word)
+    //             ->ageRange($request->age_from, $request->age_to);
+    //     })->paginate(10);
+    //     $res = UserResource::collection($partner);
+    //     $msg = "search_partner";
+    //     return $this->dataResponse($msg, $res, 200);
+    // } catch (\Exception $e) {
+    //     return $this->returnException($e->getMessage(), 500);
+    // }
 }
