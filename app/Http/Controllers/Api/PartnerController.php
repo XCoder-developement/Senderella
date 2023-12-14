2711db1912a91f989fc21a611c67a39dbb04c98a
karim el-kady <karim.elkady.kmk@gmail.com>
2023-12-14 14:33:03 +0200

    newkarim

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -299,6 +299,7 @@ class PartnerController extends Controller
             $user = auth()->user();
             $watched_ids = $user->watched->pluck("partner_id")->toArray();
             $watched = UserWatch::whereIn('id', $watched_ids)->get();
+            // dd($watched);
             $msg = "who_i_watch";
             return $this->dataResponse($msg, MiniPartnerResource::collection($watched), 200);
         } catch (\Exception $ex) {
