f69ce50f8aa2805983e0ff38cb55dae578a40318
karim el-kady <karim.elkady.kmk@gmail.com>
2023-12-20 11:57:34 +0200

    newkarim

--- app/Http/Resources/Api/PartnerResource.php
+++ app/Http/Resources/Api/PartnerResource.php
@@ -32,17 +32,17 @@ class PartnerResource extends JsonResource
             "height" => $this->height ?? "",
             "country_id" => intval($this->country_id) ?? "",
             "state_id" => intval($this->state_id) ?? "",
-            "country_title" => $this->country->title ?? "",
-            "state_title" => $this->state->title ?? "",
+            "country_title" => $this->country?->title ?? "",
+            "state_title" => $this->state?->title ?? "",
 
             "marital_status_id" => intval($this->marital_status_id) ?? null,
             "readiness_for_marriages_id" => intval($this->readiness_for_marriages_id) ?? null,
-            "marital_status_title" => $this->marital_status->title ?? "",
+            "marital_status_title" => $this->marital_status?->title ?? "",
 
             "skin_color_id" => intval($this->color_id) ?? null,
             "education_type_id" => intval($this->education_type_id) ?? null,
-            "skin_color_title" => $this->color->title ?? "",
-            "education_type_title" => $this->education_type->title ?? "",
+            "skin_color_title" => $this->color?->title ?? "",
+            "education_type_title" => $this->education_type?->title ?? "",
 
             "active" => intval($this->active) ?? "",
             "partner_more_info" => UserInformationResource::collection($this->informations),
