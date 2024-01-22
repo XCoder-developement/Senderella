b032e3d97268f46b799ed9c148ce7b74f4a163d3
OmniaSwaidan <omniaahmed129999@gmail.com>
2024-01-22 10:55:23 +0200

    add

--- resources/views/admin_dashboard/Notifications/index.blade.php
+++ resources/views/admin_dashboard/Notifications/index.blade.php
@@ -69,7 +69,7 @@
   <button type="submit" class="btn btn-shadow btn-primary font-weight-bold mt-5">
           {{__('messages.save')}}
            <span class="svg-icon svg-icon m-0 svg-icon-md">
-    			<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Angle-double-left.svg-->
+    			<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Angle-double-left.svg i-->
     			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
     				<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
     					<polygon points="0 0 24 0 24 24 0 24"></polygon>
