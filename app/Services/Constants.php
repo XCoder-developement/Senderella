aee84585a0d5ba3de40b0abf6088e0e2bf27ce03
karim el-kady <karim.elkady.kmk@gmail.com>
2024-01-25 12:21:45 +0200

    karim

--- app/Services/Constants.php
+++ app/Services/Constants.php
@@ -4,5 +4,7 @@ namespace App\Services;
 
 class Constants
 {
-  const NOTIFICATION_KEY = 'AAAAyyhCrck:APA91bGVL8jV60PLexarNcGt7eshE1WjJWP6U3T3g7zYriccXZwHlbAhnO_Ip-eyh-5ZDMy9444HQUj0zNKdSrJjWhwIntxi0Ynac8I590UE_4wwSbpryMPf1UtgDkx96YZz0b4CECAi';
+//   const NOTIFICATION_KEY = 'AAAAyyhCrck:APA91bGVL8jV60PLexarNcGt7eshE1WjJWP6U3T3g7zYriccXZwHlbAhnO_Ip-eyh-5ZDMy9444HQUj0zNKdSrJjWhwIntxi0Ynac8I590UE_4wwSbpryMPf1UtgDkx96YZz0b4CECAi';
+  const NOTIFICATION_KEY = 'AAAAC0sn-aE:APA91bGu_WwGma-HWgCYWeWKTOoQ9VXxpvTzp3ZIR_Fk-BAQ6Qeo_fCMvtT89T_f-1YPgt4OcDfK-gpj1yOHQYpMIdkfluo-GWt2HxnynOJmkvT2_jEvVsL2V_w5JekDT1Jlp7pDaYTo';
+
 }
