1d8f2390ce811605d47d9e54d3f570189839f3a5
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2024-02-04 10:55:51 +0200

    user resource

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -59,7 +59,7 @@ class UserResource extends JsonResource
 
             // "is_verified" => $this->is_verified ?? "",
             "active" => intval($this->active) ?? "",
-            "last_active" => $this->last_shows !== null && $this->last_shows->first() ? $this->last_shows?->first()?->end_date : 'active now',
+            "last_active" => $this->last_shows !== null && $this->last_shows->first() ? $this->last_shows?->first()?->end_date : '',
 
             "api_token" => $this->api_token ?? "",
             "device_token" => $this->user_device->device_token ?? "",
