42ab12d58c96cfa6098ceacc96262d370a00cfff
karim el-kady <karim.elkady.kmk@gmail.com>
2024-02-21 15:01:42 +0200

    paginate

---
+++ database/migrations/2024_02_21_091009_create_banners_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type')->default(2)->comment("to check if it's user or banner => 2");
+            $table->string('link')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
