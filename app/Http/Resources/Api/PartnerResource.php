ee622e113afdea1bd5504cf098a9e8efe0e41148
karim el-kady <karim.elkady.kmk@gmail.com>
2024-02-21 15:49:40 +0200

    banner

--- app/Http/Resources/Api/PartnerResource.php
+++ app/Http/Resources/Api/PartnerResource.php
@@ -70,51 +70,61 @@ class PartnerResource extends JsonResource
         $lat2 = $this->latitude;
         $lon2 = $this->longitude;
         if ($lat1 == null && $lon1 == null && $lat2 == null && $lon2 == null) {
-            $distance = 0 ;
-        }else{
+            $distance = 0;
+        } else {
             $distance = calculateDistance($lat1, $lon1, $lat2, $lon2);
             $distance = number_format($distance, 2);
         }
-        return [
-            "id" => $this->id,
-            "is_verify" => $this->is_verify ?? 0,
-            "active" => intval($active) ?? "",
-            "last_active" => $last_active ?? '', // $this->last_shows !== null && $this->last_shows->first() ? $this->last_shows?->first()?->end_date : '',
-            "images" => $this->images == null ? null : ImageResource::collection($images),
-            "name" => $this->name ?? "",
-            "age" => $this->user_age ?? "",
-            "is_follow" => $this->is_follow($user->id) ?? 0,
-            "trusted" => $this->trusted ?? 0,
-            "is_new" => intval(($user_duration) < $duration),
-            "notes" => $this->notes ?? __("messages.not_answered"),
-            "is_married_before" => intval($this->is_married_before) ?? 0,
+        if ($this->type == 1) {
+            return [
+                "id" => $this->id,
+                "is_verify" => $this->is_verify ?? 0,
+                "active" => intval($active) ?? "",
+                "last_active" => $last_active ?? '', // $this->last_shows !== null && $this->last_shows->first() ? $this->last_shows?->first()?->end_date : '',
+                "images" => $this->images == null ? null : ImageResource::collection($images),
+                "name" => $this->name ?? "",
+                "age" => $this->user_age ?? "",
+                "is_follow" => $this->is_follow($user->id) ?? 0,
+                "trusted" => $this->trusted ?? 0,
+                "is_new" => intval(($user_duration) < $duration),
+                "notes" => $this->notes ?? __("messages.not_answered"),
+                "is_married_before" => intval($this->is_married_before) ?? 0,
 
-            "weight" => intval($this->weight) ?? "",
-            "height" => intval($this->height) ?? "",
-            "country_id" => intval($this->country_id) ?? "",
-            "flag"  => $this->country?->image_link ?? "",
-            "state_id" => intval($this->state_id) ?? "",
-            "country_title" => $this->country?->title ?? "",
-            "state_title" => $this->state?->title ?? "",
-            "nationality_title" => $this->country?->title ?? "",
-            "distance" => intval($distance) ?? '',
-            "marital_status_id" => intval($this->marital_status_id) ?? null,
-            "readiness_for_marriages_id" => intval($this->readiness_for_marriages_id) ?? null,
-            "marital_status_title" => $this->marital_status?->title ?? "",
-            "marriage_readiness_title" => $this->marriage_readiness?->title ?? "",
+                "weight" => intval($this->weight) ?? "",
+                "height" => intval($this->height) ?? "",
+                "country_id" => intval($this->country_id) ?? "",
+                "flag"  => $this->country?->image_link ?? "",
+                "state_id" => intval($this->state_id) ?? "",
+                "country_title" => $this->country?->title ?? "",
+                "state_title" => $this->state?->title ?? "",
+                "nationality_title" => $this->country?->title ?? "",
+                "distance" => intval($distance) ?? '',
+                "marital_status_id" => intval($this->marital_status_id) ?? null,
+                "readiness_for_marriages_id" => intval($this->readiness_for_marriages_id) ?? null,
+                "marital_status_title" => $this->marital_status?->title ?? "",
+                "marriage_readiness_title" => $this->marriage_readiness?->title ?? "",
 
-            "skin_color_id" => intval($this->color_id) ?? null,
-            "education_type_id" => intval($this->education_type_id) ?? null,
-            "skin_color_title" => $this->color?->title ?? "",
-            "education_type_title" => $this->education_type?->title ?? "",
+                "skin_color_id" => intval($this->color_id) ?? null,
+                "education_type_id" => intval($this->education_type_id) ?? null,
+                "skin_color_title" => $this->color?->title ?? "",
+                "education_type_title" => $this->education_type?->title ?? "",
 
-            "visibility"   => intval($this->visibility),
+                "visibility"   => intval($this->visibility),
 
-            "like_time" => $like_time ?? '',
-            "favorite_time" => $favorite_time ?? '',
-            "watch_time" => $watch_time ?? '',
-            // "partner_more_info" => UserInformationResource::collection($this->informations),
-        ];
+                "like_time" => $like_time ?? '',
+                "favorite_time" => $favorite_time ?? '',
+                "watch_time" => $watch_time ?? '',
+                "type" => intval($this->type) ?? '',
+                // "partner_more_info" => UserInformationResource::collection($this->informations),
+            ];
+        } else if ($this->type == 2) {
+            return[
+                "id" => $this->id ?? "" ,
+                "link" =>$this->link ?? "",
+                "image" => $this->image_link ?? "",
+                "type"  => intval($this->type) ?? '',
+            ];
+        }
     }
 }
 
