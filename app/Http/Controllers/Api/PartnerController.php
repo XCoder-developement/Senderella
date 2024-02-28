ae89ddbcae758eb0654da7dfe48598e87bbd97c2
karim el-kady <karim.elkady.kmk@gmail.com>
2024-02-28 17:40:42 +0200

    most_liked

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -825,8 +825,9 @@ class PartnerController extends Controller
             // $mostLikedPartnerId = array_diff($mostLikedPartnerIds, $blocked);
             // dd($mostLikedPartnerIds);
 
-            $mostLikedCount = User::whereIn('id', array_keys($mostLikedPartnerIds))->whereNotIn('id', $blocked)->where('gender', '!=', $user->gender);
-            dd($mostLikedCount);
+            $mostLikedCount = User::whereIn('id', array_keys($mostLikedPartnerIds))->whereNotIn('id', $blocked)
+            // ->where('gender', '!=', $user->gender)
+            ;
             $mostLikedCount = $mostLikedCount->paginate(10);
 
             if ($banner1 || $text_banner) {
