4a7e19c65f483249c43499b8e8a8224e941f1c6a
karim el-kady <karim.elkady.kmk@gmail.com>
2024-04-15 17:16:18 +0200

    karim

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -146,7 +146,6 @@ class UserController extends Controller
                         if($user_information["requirment_item_id"]){
                     $requirment_item_id = $user_information["requirment_item_id"];
                             // $count++;
-
                         }else{
                             $requirment_item_id = 'messages.not_answered';
                         }
