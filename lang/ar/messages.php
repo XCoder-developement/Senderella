7285e77918befe35fa7d4e8182187d065fce3c5e
karim el-kady <karim.elkady.kmk@gmail.com>
2024-04-16 10:40:53 +0200

    karim

--- lang/ar/messages.php
+++ lang/ar/messages.php
@@ -473,7 +473,7 @@ return [
     "new_partner_activity"    => "تم النشر بنجاح",
     "your account is not verified"    => "عفوا انت غير مشترك",
     "Your chats"    => "محادثاتك",
-    "password_is_incorrect" => "كلمة المرور غير صاحيحة",
+    "password_is_incorrect" => "كلمة المرور غير صحيحة",
     "done_updating_visibility" => 'تم تحديث خاصية الاظهار',
     "message"    => "رسالة",
     "done_updating_location"    => "تم تحديث موقعك",
