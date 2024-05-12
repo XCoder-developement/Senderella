67c20399d534e09a08893c9ab5b413a5e7857227
OmniaSwaidan <omniaahmed129999@gmail.com>
2024-05-12 17:05:41 +0300

    add

--- app/Http/Resources/Api/ChatResource.php
+++ app/Http/Resources/Api/ChatResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Api;
 
 use App\Models\Chat\Chat;
 use App\Models\Chat\ChatMessage;
+use App\Models\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,12 +17,20 @@ class ChatResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $user = auth()->user();
+        $chat_parts = $this->chat_users->pluck('user_id')->toArray();
+        $user = User::whereIn('id', $chat_parts)->whereNot('id',auth()->id())->first();
+        // $user = auth()->user();
         $message = ChatMessage::where('chat_id', $this->id)->orderBy('created_at', 'desc')->value('message');
+        $last_message = ChatMessage::where('chat_id', $this->id)->orderBy('created_at', 'desc')->first();
         return [
             'chat_id' => $this->id,
             'name'   => $this->name ?? '',
             'message'   => $message ?? '',
+            'date' => $last_message?->created_at->format('Y-m-d') ?? '',
+            'time' => $last_message?->created_at->format('g:i A') ?? '',
+            'partner' => new PartnerResource($user),
+
+
             // $message = $user->is_verify == 1 ? $message : substr($message, 0, 5) . encrypt(substr($message, 5)),
         ];
     }
