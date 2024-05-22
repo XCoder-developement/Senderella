aed09baa70b32b9ace1f5ce84e2090f3e8f607a4
karim el-kady <karim.elkady.kmk@gmail.com>
2024-05-22 12:04:44 +0300

    edit chat controller

--- app/Services/SendNotification.php
+++ app/Services/SendNotification.php
@@ -24,7 +24,6 @@ class SendNotification
                     "click_action" => "FLUTTER_NOTIFICATION_CLICK"
                 ],
         ];
-        dd($data);
         $dataString = json_encode($data);
         $headers = [
             'Authorization: key=' . Constants::NOTIFICATION_KEY,
