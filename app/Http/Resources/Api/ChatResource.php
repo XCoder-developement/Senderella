548f9cf635a3785546cf4d435a1fa84ea54fb723
karim el-kady <karim.elkady.kmk@gmail.com>
2024-05-22 16:47:35 +0300

    edit chat controller

--- app/Http/Resources/Api/ChatResource.php
+++ app/Http/Resources/Api/ChatResource.php
@@ -24,7 +24,7 @@ class ChatResource extends JsonResource
         $auth = User::find(auth()->id() );
         $message = ChatMessage::where('chat_id', $this->id)->orderBy('created_at', 'desc')->value('message');
         $last_message = ChatMessage::where('chat_id', $this->id)->orderBy('created_at', 'desc')->first();
-        $is_blocked = UserBlock::where('user_id', auth()->id())->where('partner_id', $user->id)->first();
+        $is_blocked = UserBlock::where('user_id', auth()->id())->where('partner_id', auth()->id())->first();
 
         return [
             'chat_id' => $this->id,
@@ -38,7 +38,7 @@ class ChatResource extends JsonResource
             'partner' => new PartnerResource($user),
             'show_my_image' => $this->chat_users->where('user_id',auth()->id())->first()?->image_status ?? 0,
             'show_user_image' => $this->chat_users->where('user_id','!=',auth()->id())->first()?->image_status ?? 0,
-            'is_blocked' => $is_blocked ?? 0,
+            'is_blocked' => $is_blocked? 1 : 0,
             // $message = $user->is_verify == 1 ? $message : substr($message, 0, 5) . encrypt(substr($message, 5)),
         ];
     }
