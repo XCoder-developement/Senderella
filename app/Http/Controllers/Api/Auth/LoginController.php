c681587219d562a1a5c80495b808c412771fc2a8
karim el-kady <karim.elkady.kmk@gmail.com>
2024-06-02 12:27:47 +0300

    edit login function in login controller

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\Api\UserResource;
 use Illuminate\Http\Request;
 use App\Models\User\User;
 use App\Traits\ApiTrait;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Validator;
 
@@ -50,7 +51,8 @@ class LoginController extends Controller
                ]);
             // }
          //add device to user
-
+               Auth::logoutOtherDevices($request->password);
+               
          $userDevice = $user->user_devices()->firstOrNew([
             'device_type' => $request->device_type,
             'device_id' => $request->device_id,
